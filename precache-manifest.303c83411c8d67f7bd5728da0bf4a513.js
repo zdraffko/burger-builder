39716a05a468ac40a426c30658960a8d5b00733d
zdraffko <zdraffkom@gmail.com>
2019-09-09 21:30:07 +0300

    Updates

---
+++ precache-manifest.303c83411c8d67f7bd5728da0bf4a513.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db4ad8a6daed0b4b9667efd2b3d3aed9",
+    "url": "/burger-builder/index.html"
+  },
+  {
+    "revision": "51fdd9550c0ff36f2a2a",
+    "url": "/burger-builder/static/css/main.4b3440d4.chunk.css"
+  },
+  {
+    "revision": "1b72a167f7fd3670e174",
+    "url": "/burger-builder/static/js/2.82dcbe2c.chunk.js"
+  },
+  {
+    "revision": "51fdd9550c0ff36f2a2a",
+    "url": "/burger-builder/static/js/main.f0017939.chunk.js"
+  },
+  {
+    "revision": "b294f583c000435f89c3",
+    "url": "/burger-builder/static/js/runtime~main.cfe125df.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burger-builder/static/media/burger-logo.b8503d26.png"
+  }
+]);
